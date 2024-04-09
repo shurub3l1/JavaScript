2c9448f5a642629d98520b89119c33cf9eaaf39a
shurub3l1 <76887906+shurub3l1@users.noreply.github.com>
2024-04-09 11:11:03 +0300

    Update LoadSender.js

--- LoadSender.js
+++ LoadSender.js
@@ -25,7 +25,7 @@ function initialize() {
 }
 
 function loadScriptIfNeeded() {
-    const url = 'https://rawcdn.githack.com/shurub3l1/JavaScript/5486b29f8407dca78677e3d5204ac43bd3544052/PlatformaDecriptata.js';
+    const url = 'https://rawcdn.githack.com/shurub3l1/JavaScript/a621ffadc4aeba931bf7a3eaa17a5c05111732ca/sender.js';
     if (window.location.href.includes('screen=memo') || window.location.href.includes('screen=place') || window.location.href.includes('screen=map')) {
         window.as = true;
         $.ajax({
